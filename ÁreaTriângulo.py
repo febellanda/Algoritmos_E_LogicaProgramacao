cd29f84f33cb365a818a541da14103a961173dec
Felipe Bellanda <felipebellanda0808@gmail.com>
2026-08-19 09:36:58 -0300

    Create ÁreaTriângulo.py

---
+++ ÁreaTriângulo.py
@@ -0,0 +1,6 @@
+print("- ÁREA DO TRIÂNGULO -\n")
+base = int(input("Insira a base: "))
+altura = int(input("Insira a altura: "))
+area_triangulo = (base*altura)/2
+
+print(f"Área do triângulo: {area_triangulo}")
