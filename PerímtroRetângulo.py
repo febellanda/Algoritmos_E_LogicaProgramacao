8b38597ce10ff4684cbf55dd87394ea93bb9da49
Felipe Bellanda <felipebellanda0808@gmail.com>
2026-08-19 09:37:46 -0300

    Add perimeter calculation for rectangle

---
+++ PerímtroRetângulo.py
@@ -0,0 +1,6 @@
+print("- PERÍMETRO DO RETÂNGULO -\n")
+lado_a = int(input("Insira o comprimento do retângulo: "))
+lado_b = int(input("Insira a largura do retângulo: "))
+perimetro_ret = lado_a*2 + lado_b*2
+
+print(f"Perímetro do retângulo: {perimetro_ret}")
